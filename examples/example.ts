cbb5c88d53123d27efc397a1e94a2cb91e6f2b7d
peal-26 <edilasio@live.com>
2025-11-30 17:32:30 +0100

    fix: atualiza importação do SeedGenerator e adiciona dependência no package.json

--- examples/example.ts
+++ examples/example.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { PrismaClient } from './generated/prisma/client';
 import { faker } from '@faker-js/faker';
 
-import { SeedGenerator } from '../src/seed-generator'
+import { SeedGenerator } from 'seed-data-generator'
 
 /**
  * Exemplo de uso com Prisma
